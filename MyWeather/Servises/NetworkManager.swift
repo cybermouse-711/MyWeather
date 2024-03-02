7da7eb88fa4457404fc24e3284093ba9917016e7
Liza <desseobscurite@gmail.com>
2024-03-02 20:55:01 +0400

    Created metod for GetSession

--- MyWeather/Servises/NetworkManager.swift
+++ MyWeather/Servises/NetworkManager.swift
@@ -7,26 +7,7 @@
 
 import Foundation
 
-final class NetworkManager {
-    
-    static let shared = NetworkManager()
-    
-    private init() {}
-    
-    func fetchForecast() async throws -> DataWeather {
-        guard let url = URL(string: API.weatherURL.rawValue) else {
-            throw NetworkError.invalidURL
-        }
-        let (data, _) = try await URLSession.shared.data(from: url)
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        guard let forecast = try? decoder.decode(DataWeather.self, from: data) else {
-            throw NetworkError.decodingError
-        }
-        return forecast
-    }
-}
-
+//MARK: - Enums
 enum NetworkError: Error {
     case noData, invalidURL, decodingError
 }
@@ -34,3 +15,51 @@ enum NetworkError: Error {
 enum API: String {
     case weatherURL = "https://api.weather.yandex.ru/v2/informers?lat=55.7&lon=37.6"
 }
+
+//MARK: - NetworkManager
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+//    func fetchForecast() async throws -> DataWeather {
+//        guard let url = URL(string: API.weatherURL.rawValue) else {
+//            throw NetworkError.invalidURL
+//        }
+//        let (data, _) = try await URLSession.shared.data(from: url)
+//        let decoder = JSONDecoder()
+//        decoder.keyDecodingStrategy = .convertFromSnakeCase
+//        guard let forecast = try? decoder.decode(DataWeather.self, from: data) else {
+//            throw NetworkError.decodingError
+//        }
+//        return forecast
+//    }
+    
+    func fetchForecast(comletion: @escaping(Result<DataWeather, NetworkError>) -> Void) {
+        guard let url = URL(string: API.weatherURL.rawValue) else {
+            comletion(.failure(.invalidURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                comletion(.failure(.noData))
+                print(error?.localizedDescription ?? "No error description")
+                return
+                }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let dataModel = try decoder.decode(DataWeather.self, from: data)
+                DispatchQueue.main.async {
+                    comletion(.success(dataModel))
+                }
+            } catch {
+                comletion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+}
+
--- MyWeather/ViewControllers/WeatherScreenScene/WeatherScreenViewController.swift
+++ MyWeather/ViewControllers/WeatherScreenScene/WeatherScreenViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 //MARK: - WeatherScreenViewController
 final class WeatherScreenViewController: UIViewController {
     
-    private let dataWeather = NetworkManager.shared
+    //MARK: - Private properties
+    private let networkManager = NetworkManager.shared
+    private var weather: DataWeather!
     
     //MARK: - Override metods
     override func viewDidLoad() {
         super.viewDidLoad()
         view = WeatherScreenView(frame: UIScreen.main.bounds)
+    }
+    
+    //MARK: - Private metods
+    private func getForecast() {
+        networkManager.fetchForecast { result in
+            switch result {
+            case .success(let weather):
+                self.weather = weather
+                view.dataLabel.text = weather.forecast.date
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 

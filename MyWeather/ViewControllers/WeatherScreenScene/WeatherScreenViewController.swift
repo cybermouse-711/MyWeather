a64083f27e560e52c453211f633f7f91762eed28
Liza <desseobscurite@gmail.com>
2024-04-05 18:33:44 +0400

    Deleted View for VC

--- MyWeather/ViewControllers/WeatherScreenScene/WeatherScreenViewController.swift
+++ MyWeather/ViewControllers/WeatherScreenScene/WeatherScreenViewController.swift
@@ -14,10 +14,14 @@ final class WeatherScreenViewController: UIViewController {
     private let networkManager = NetworkManager.shared
     private var weather: DataWeather!
     
+    var dataLabel = UILabel()
+    private var degreesLabel = UILabel()
+    private var cityLabel = UILabel()
+    
     //MARK: - Override metods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = WeatherScreenView(frame: UIScreen.main.bounds)
+        setupUI()
     }
     
     //MARK: - Private metods
@@ -26,12 +30,86 @@ final class WeatherScreenViewController: UIViewController {
             switch result {
             case .success(let weather):
                 self.weather = weather
-                view.dataLabel.text = weather.forecast.date
+                self.dataLabel.text = weather.forecast.date
             case .failure(let error):
                 print(error)
             }
         }
     }
 }
+
+
+//MARK: - Configure UI
+private extension WeatherScreenViewController {
+    func setupUI() {
+        addSubViews()
+        
+        setupConctraints()
+        
+        setupSelfView()
+        
+        setupDataLabel()
+        setupDegreesLabel()
+        setupCityLabel()
+    }
+}
+
+//MARK: - Setup UI
+private extension WeatherScreenViewController {
+    func setupSelfView() {
+        view.backgroundColor = .systemCyan
+    }
+    
+    func addSubViews() {
+        [dataLabel, degreesLabel, cityLabel].forEach { subView in
+            view.addSubview(subView)
+        }
+    }
+    
+    func setupDataLabel() {
+        dataLabel.text = "data"
+        dataLabel.textColor = .white
+        dataLabel.font = UIFont.systemFont(ofSize: 20)
+        dataLabel.textAlignment = .center
+    }
+    
+    func setupDegreesLabel() {
+        degreesLabel.text = "degrees"
+        degreesLabel.textColor = .white
+        degreesLabel.font = UIFont.systemFont(ofSize: 35)
+        degreesLabel.textAlignment = .center
+    }
+    
+    func setupCityLabel() {
+        cityLabel.text = "city"
+        cityLabel.textColor = .white
+        cityLabel.font = UIFont.systemFont(ofSize: 25)
+        cityLabel.textAlignment = .center
+    }
+}
+
+//MARK: - Constraints
+private extension WeatherScreenViewController {
+    func setupConctraints() {
+        [dataLabel, degreesLabel, cityLabel].forEach { subView in
+            subView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                dataLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+                dataLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                dataLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                
+                degreesLabel.topAnchor.constraint(equalTo: dataLabel.topAnchor, constant: 60),
+                degreesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                degreesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                
+                cityLabel.topAnchor.constraint(equalTo: degreesLabel.topAnchor, constant: 60),
+                cityLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                cityLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            ])
+        }
+    }
+}
+
 
 

7721c9767e8f17a35ca42e85d636f833f07d6d79
Liza <desseobscurite@gmail.com>
2024-04-05 18:45:26 +0400

    Created file MVXProtocole

---
+++ MyWeather/Servises/MVXProtocole.swift
@@ -0,0 +1,16 @@
+//
+//  MVXProtocole.swift
+//  MyWeather
+//
+//  Created by Elizaveta Medvedeva on 05.04.24.
+//
+
+import UIKit
+
+protocol BaseAssembly {
+    func configure(viewController: UIViewController)
+}
+
+protocol BaseRouting {
+    func routeTo(Target: Any)
+}

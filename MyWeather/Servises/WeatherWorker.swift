7744298b6d43d694ce850bb398718733b0ba70fc
Liza <desseobscurite@gmail.com>
2024-04-11 18:51:09 +0400

    Created Worker

---
+++ MyWeather/Servises/WeatherWorker.swift
@@ -0,0 +1,57 @@
+//
+//  WeatherWorker.swift
+//  MyWeather
+//
+//  Created by Elizaveta Medvedeva on 05.04.24.
+//
+
+import UIKit
+import Alamofire
+
+//MARK: - Enums
+enum NetworkError: Error {
+    case noData, invalidURL, decodingError
+}
+
+//MARK: - Protocols
+protocol IWeatherWorker {
+    func fetchForecast(completion: @escaping(Result<DataWeather, Error>) -> Void)
+}
+
+//MARK: - WeatherRouter
+final class WeatherWorker {
+    
+    private let viewController: UIViewController
+    
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+}
+
+//MARK: - Extensions for protocol
+extension WeatherWorker: IWeatherWorker {
+    func fetchForecast(completion: @escaping (Result<DataWeather, Error>) -> Void) {
+        let url = "https://api.spacexdata.com/v5/launches/latest"
+        
+        AF.request(url)
+            .validate()
+            .response { response in
+                guard let data = response.data else {
+                    if let error = response.error {
+                        completion(.failure(NetworkError.noData))
+                    }
+                    return
+                }
+                
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                guard let dataWeather = try? decoder.decode(DataWeather.self, from: data) else {
+                    completion(.failure(NetworkError.invalidURL))
+                    return
+                }
+                completion(.success(dataWeather))
+            }
+    }
+}
+
